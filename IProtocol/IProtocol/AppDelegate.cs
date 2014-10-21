efd57d64c0abc71201dc417c7c8ee762aca3b910
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-10-21 19:10:03 +0400

    added IProtocoll test

---
+++ IProtocol/IProtocol/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace IProtocol
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			window.MakeKeyAndVisible ();
+			return true;
+		}
+	}
+}
+
---
+++ IProtocol/IProtocol/MainViewController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UIKit;
+
+namespace IProtocol
+{
+	public class MainViewController : UIViewController, IUIScrollViewDelegate
+	{
+		public MainViewController ()
+		{
+		}
+
+//		override Scrolle
+	}
+}
+
