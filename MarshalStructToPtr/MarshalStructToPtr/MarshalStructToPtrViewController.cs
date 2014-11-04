3d41dc683c10024ac3fe741f49e77e847ee3ac5b
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-11-04 19:38:36 +0300

    Struct to prt test

---
+++ MarshalStructToPtr/MarshalStructToPtr/MarshalStructToPtrViewController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+using System.Runtime.InteropServices;
+
+namespace MarshalStructToPtr
+{
+	public partial class MarshalStructToPtrViewController : UIViewController
+	{
+		[StructLayout(LayoutKind.Sequential, Pack=1)]
+		struct AVAudioTapProcessorContext
+		{
+			public bool SupportedTapProcessingFormat;
+
+			public bool IsNonInterleaved;
+
+			public double SampleRate;
+
+			public double SampleCount;
+
+			public float LeftChannelVolume;
+
+			public float RightChannelVolume;
+
+			public IntPtr AudioUnit;
+
+			public IntPtr Self;
+		}
+
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public MarshalStructToPtrViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			AVAudioTapProcessorContext context = new AVAudioTapProcessorContext {
+				SupportedTapProcessingFormat = false,
+				IsNonInterleaved = false,
+				SampleRate = double.NaN,
+				SampleCount = 0,
+				LeftChannelVolume = 0,
+				RightChannelVolume = 0
+			};
+
+			int size = Marshal.SizeOf (typeof(AVAudioTapProcessorContext));
+			IntPtr handle = Marshal.AllocHGlobal (size);
+			context.Self = handle;
+			Marshal.StructureToPtr<AVAudioTapProcessorContext> (context, handle, false);
+
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
