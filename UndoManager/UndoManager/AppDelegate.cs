7f1a83ae4497b066e0483108d54138ab249d2f40
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-02-10 20:32:39 +0300

    show problem with undoManager.PrepareWithInvocationTarget

---
+++ UndoManager/UndoManager/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace UndoManager
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to
+	// application events from iOS.
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+		MainController controller;
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			controller = new MainController ();
+
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			window.RootViewController = controller;
+			window.MakeKeyAndVisible ();
+
+			return true;
+		}
+	}
+}
+
---
+++ UndoManager/UndoManager/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+namespace UndoManager
+{
+	public class MainController : UIViewController
+	{
+		NSUndoManager undoManager;
+
+		public MainController ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			View.BackgroundColor = UIColor.Cyan;
+
+			undoManager = new NSUndoManager ();
+
+			NSObject target = undoManager.PrepareWithInvocationTarget (this);
+			Console.WriteLine (target);
+		}
+	}
+}
+
