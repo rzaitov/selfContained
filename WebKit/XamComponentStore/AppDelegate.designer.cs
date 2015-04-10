4a157c43e01d657fcb32d1053083b0b767521a55
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-04-10 19:32:16 +0300

    add XamarinMac version

---
+++ WebKit/XamComponentStore/AppDelegate.designer.cs
@@ -0,0 +1,7 @@
+﻿namespace XamComponentStore
+{
+	[global::Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
---
+++ WebKit/XamComponentStore/MainWindowController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Foundation;
+using AppKit;
+
+namespace XamComponentStore
+{
+	public partial class MainWindowController : NSWindowController
+	{
+		public MainWindowController (IntPtr handle) : base (handle)
+		{
+		}
+
+		[Export ("initWithCoder:")]
+		public MainWindowController (NSCoder coder) : base (coder)
+		{
+		}
+
+		public MainWindowController () : base ("MainWindow")
+		{
+		}
+
+		public override void AwakeFromNib ()
+		{
+			base.AwakeFromNib ();
+
+			WebView.MainFrame.LoadRequest (new NSUrlRequest (NSUrl.FromString ("http://components.xamarin.com/")));
+		}
+
+		public new MainWindow Window {
+			get { return (MainWindow)base.Window; }
+		}
+	}
+}
