27e48ead1af3d6ba456a06523bedd345bf98b43f
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-05-05 18:57:58 +0300

    test for interface members

---
+++ RenameInterfaceMember/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RenameInterfaceMember
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+		}
+	}
+
+	public interface ISimple
+	{
+		void SimpleMehod ();			// Rename me!!
+		bool SimpleProp { get; }		// Rename me!!
+	}
+
+	public class SimpleImpl : ISimple
+	{
+		public void SimpleMehod ()		// I will not change
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool SimpleProp {		// I will not change my name
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+	}
+}
