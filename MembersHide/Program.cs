1e8b41fa0d1797d0353d6d2baa0586948ae398bf
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-05-20 16:20:27 +0300

    test for "rename" refactoring

---
+++ MembersHide/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MembersHide
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+
+	class Base
+	{
+		public void CoolMethod ()
+		{
+			
+		}
+
+		public void AnotherCoolMethod ()
+		{
+			
+		}
+	}
+
+	class Derived : Base
+	{
+		// Rename me with "Cmd + R"
+		public void CoolMethod ()
+		{
+			
+		}
+
+		// Rename me with "Cmd + R"
+		public new void AnotherCoolMethod ()
+		{
+
+		}
+	}
+}
