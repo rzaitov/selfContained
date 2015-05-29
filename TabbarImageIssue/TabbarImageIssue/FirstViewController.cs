94a42fb4e4945267b11586da49b885abed974b26
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-05-30 02:07:09 +0300

    initial commit for TabbarImageIssue sample

---
+++ TabbarImageIssue/TabbarImageIssue/FirstViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+
+namespace TabbarImageIssue
+{
+	public partial class FirstViewController : UIViewController
+	{
+		public FirstViewController (IntPtr handle) : base (handle)
+		{
+			TabBarItem.Title = "First";
+			TabBarItem.Image = UIImage.FromBundle ("tabIconMessages");
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ TabbarImageIssue/TabbarImageIssue/SecondViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UIKit;
+
+namespace TabbarImageIssue
+{
+	public partial class SecondViewController : UIViewController
+	{
+		public SecondViewController (IntPtr handle) : base (handle)
+		{
+			TabBarItem.Title = "Second";
+			TabBarItem.Image = UIImage.FromBundle ("tabIconPoints");
+		}
+	}
+}
+
