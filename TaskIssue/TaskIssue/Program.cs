81a2320e0dce9705cfabec20e9d7d25863c332a3
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-07-07 01:59:40 +0300

    Pinvoke issue with threadpool

---
+++ TaskIssue/TaskIssue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace TaskIssue
+{
+	class MainClass
+	{
+		[DllImport ("libc")]
+		private static extern int getpid ();
+
+		[DllImport ("libclang", CallingConvention = CallingConvention.Cdecl, EntryPoint = "clang_createIndex")]
+		public static extern IntPtr createIndex (int excludeDeclarationsFromPCH, int displayDiagnostics);
+
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (createIndex (1, 1));
+			Console.WriteLine (getpid());
+
+			Task.Factory.StartNew (() => {
+				Console.WriteLine ("Hello world");
+			});
+
+			Thread.Sleep (1000);
+		}
+	}
+}
