5d782298f5bf0af355bd7da5957e7dd21df8da42
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-07-27 16:49:48 +0300

    add custom cell

---
+++ CustomCellXI/CustomCellXI/CustomCell.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CustomCellXI
+{
+	partial class CustomCell : UITableViewCell
+	{
+		public CustomCell (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
