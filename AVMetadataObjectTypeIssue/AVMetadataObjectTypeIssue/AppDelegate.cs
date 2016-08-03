1d48dcf7ed1850fb132c6922a0342404518e50f2
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-03 23:22:05 +0300

    self contained example for AVMetadataObjectType

---
+++ AVMetadataObjectTypeIssue/AVMetadataObjectTypeIssue/AppDelegate.cs
@@ -0,0 +1,28 @@
+﻿using AVFoundation;
+using Foundation;
+using UIKit;
+
+namespace AVMetadataObjectTypeIssue
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window {
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			var output = new AVCaptureMetadataOutput (); //AVMetadataObjectType
+			output.MetadataObjectTypes = AVMetadataObjectType.Face | AVMetadataObjectType.AztecCode;
+			System.Console.WriteLine (output.MetadataObjectTypes);
+			return true;
+		}
+	}
+}
+
