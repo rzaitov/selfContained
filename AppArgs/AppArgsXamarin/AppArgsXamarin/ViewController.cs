23eba59f161cf7c04b3f0d56bf7a865dfabfdff1
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-10 21:20:46 +0300

    add XI sample

---
+++ AppArgs/AppArgsXamarin/AppArgsXamarin/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace AppArgsXamarin
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		[Export ("initWithCoder:")]
+		public ViewController (NSCoder coder)
+			: base (coder)
+		{
+			
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
