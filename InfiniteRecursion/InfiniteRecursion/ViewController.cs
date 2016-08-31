da73dd3e91698d740f3c99e8972ee2c647ffdb81
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-31 17:24:20 +0300

    InfiniteRecursion

---
+++ InfiniteRecursion/InfiniteRecursion/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace InfiniteRecursion
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			MethodWithInfiniteRecursion ();
+		}
+
+		void MethodWithInfiniteRecursion ()
+		{
+			var trueCondition = "123".Length == 3;
+			if (trueCondition)
+				MethodWithInfiniteRecursion ();
+		}
+	}
+}
