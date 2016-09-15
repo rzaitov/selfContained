3be2b240f77ae9df52c9cfa1d7dd18612da15cf1
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-09-15 23:58:32 +0300

    show syntax highliting issue within shared projects

---
+++ SharedSyntaxIssue/Shared/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+
+using UIKit;
+
+namespace Shared
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window { get; set; }
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			#if __IOS__
+			Console.WriteLine ("this is iOS");
+			#elif __tvOS__
+			Console.WriteLine ("this is tvOS");
+			#endif
+			return true;
+		}
+	}
+}
+
