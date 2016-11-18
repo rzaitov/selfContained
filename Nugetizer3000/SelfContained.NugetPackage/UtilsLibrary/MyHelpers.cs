f7b3b5c8543d5a97d0e939737d2fe9edc5fe3069
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-11-18 16:45:43 +0300

    initial commit

---
+++ Nugetizer3000/SelfContained.NugetPackage/UtilsLibrary/MyHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UtilsLibrary
+{
+	public class MyHelpers
+	{
+		public string GetString ()
+		{
+			return "hello from net45 library";
+		}
+	}
+}
---
+++ Nugetizer3000/SelfContained.NugetPackage/readme.txt
@@ -0,0 +1,2 @@
+﻿Let's checkt that this text is actually displayed
+This readme.txt file will be displayed immediately after the NuGet package is installed.
