8ba1cdb2aa1fb56a57e9ee6dc4112dcf30110e3d
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-12-21 01:46:11 +0300

    nunit config file

---
+++ CoolLib/CoolLib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoolLib
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ CoolLib/LibTests/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using NUnit.Framework;
+
+namespace LibTests
+{
+	[TestFixture]
+	public class MyClass
+	{
+		[Test]
+		public void SimpleTest ()
+		{
+			ConnectionStringSettings conStr = ConfigurationManager.ConnectionStrings ["TestDatabase_ConnecionString"];
+			Assert.AreEqual ("this is test db connection string", conStr.ConnectionString);
+		}
+	}
+}
