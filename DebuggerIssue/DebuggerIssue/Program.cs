cb685979b8ac73e47ec50355c01adad314f30b57
Rustam Zaitov <rustam.zaitov@xamarin.com>
2017-02-14 16:34:29 +0300

    DebuggerIssue initial commit

---
+++ DebuggerIssue/DebuggerIssue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace DebuggerIssue
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			var fb = Builders<Person>.Filter;
+			var ub = Builders<Person>.Update;
+
+			var filter = fb.Eq (p => p.Name, "rustam");
+
+			Console.WriteLine (filter);
+
+			string tmp = "name";
+			Console.WriteLine (tmp);
+
+		}
+	}
+
+	public class Person
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
