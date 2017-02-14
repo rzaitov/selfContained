e1ea1fa78385be67f126775eeaea43715bdf4c97
Rustam Zaitov <rustam.zaitov@xamarin.com>
2017-02-14 16:42:48 +0300

    print to console

--- DebuggerIssue/DebuggerIssue/Program.cs
+++ DebuggerIssue/DebuggerIssue/Program.cs
@@ -7,16 +7,19 @@ namespace DebuggerIssue
 	{
 		public static void Main (string [] args)
 		{
+			Console.WriteLine ("Step1. Init");
 			var fb = Builders<Person>.Filter;
 			var ub = Builders<Person>.Update;
 
+			Console.WriteLine ("Step2. Use expressions");
 			var filter = fb.Eq (p => p.Name, "rustam");
 
+			Console.WriteLine ("Step3. print filter to console (avoid unused vars)");
 			Console.WriteLine (filter);
 
+			Console.WriteLine ("Step4. some boilerplate");
 			string tmp = "name";
 			Console.WriteLine (tmp);
-
 		}
 	}
 
